abb530f9fa036677599581e1e9f060072a68d9bc
sadmen rashik khan <sadmenrashik06@gmail.com>
2022-10-20 20:30:18 +0600

    card and news components

--- src/Page/News/News/News.jsx
+++ src/Page/News/News/News.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Button, Card } from 'react-bootstrap';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const News = () => {
+    const news = useLoaderData();
+    console.log(news);
+    const { title, details, image_url, category_id } = news;
     return (
-        <div>
-            <h2>News</h2>
-        </div>
+        <Card>
+        <Card.Img variant="top" src={image_url} />
+        <Card.Body>
+            <Card.Title>{title}</Card.Title>
+            <Card.Text>
+                {details}
+            </Card.Text>
+            <Link to={`/category/${category_id}`}>
+                <Button variant="primary">All news in this category</Button>
+            </Link>
+        </Card.Body>
+    </Card>
     );
 };
 
